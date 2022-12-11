7170ed589eb67056faa63e031e166abb82964418
Hilmi <70850136+hilsky@users.noreply.github.com>
2022-12-11 10:19:56 +0700

    Update forecasting1.py

--- forecasting1.py
+++ forecasting1.py
@@ -3,17 +3,3 @@ import pandas as pd
 df = pd.read_csv("AirPassengers.csv")
 
 print(df.tail())
-
-# Jawaban soal pertama
-lastDate = df["Month"].tail().max()
-print("Data terakhir berada pada tahun:", lastDate)
-
-# Jawaban soal kedua
-maxPassengers = df[["Month", "#Passengers"]].tail().max()
-print("Penumpang terbanyak berada pada tahun:", maxPassengers)
-
-
-# Jawaban soal ketiga
-totalPassenger = df['#Passengers'].tail().sum()
-
-print("Total keseluruhan penumpang berjumlah:", totalPassenger)
